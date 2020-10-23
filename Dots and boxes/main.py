73c4051a165e7e19274cf582abb0da972a41247d
TheIcySpark <saacmanjarrez@gmail.com>
2020-10-23 10:10:18 -0500

    dots and bosx

--- Dots and boxes/main.py
+++ Dots and boxes/main.py
@@ -1,8 +1,14 @@
+import sys
+
 class Caja:
     posicion = ""
     lados = ""
+    def __init__(self, posicion, lados):
+        self.posicion = posicion
+        self.lados = lados
 
 tam_tablero, id_jugador, puntos_jugador, puntos_oponente, numero_cajas = 0, 0, 0, 0, 0
+cajas = []
 
 def lectura_inicial():
     tam_tablero = int(input())
@@ -11,12 +17,24 @@ def lectura_inicial():
 
 
 def lectura_turno():
+    global cajas
     puntos_jugador, puntos_oponente = [int(i) for i in input().split()]
     numero_cajas= int(input())
+    cajas = []
     for i in range(numero_cajas):
-        box, sides = input().split()
+        posicion, lados = input().split()
+        caja = Caja(posicion, lados)
+        cajas.append(caja)
 
+def hacer_jugada():
+    global cajas
+    if len(cajas[-1].lados) == 1:
+        print(str(cajas[-1].posicion) + " " + str(cajas[-1].lados))
+    else:
+        print(str(cajas[0].posicion) + " " + str(cajas[0].lados[0]))
 
 lectura_inicial()
 while True:
     lectura_turno()
+    cajas = sorted(cajas, key = lambda Caja: len(Caja.lados), reverse = True)
+    hacer_jugada()
